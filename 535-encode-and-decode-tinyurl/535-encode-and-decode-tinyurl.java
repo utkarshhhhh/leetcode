f7097099830f06209e49151987c0e23282eb25a7
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-04-23 16:00:14 +0530

    Time: 14 ms (13.57%), Space: 43.8 MB (30.07%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,13 +1,21 @@
 public class Codec {
 
+    HashMap<Integer,String> map = new HashMap<>();
+    String host = "http://tinyurl.com/" ; 
+    
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        
+        int hashcode = longUrl.hashCode(); 
+        map.put( hashcode , longUrl );
+        return host+hashcode;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;    
+        
+      int key = (int)Integer.parseInt(shortUrl.replace(host,""));
+        return map.get(key);    
     }
 }
 
