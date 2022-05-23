afabad61e3aa7d940a0d5395dba88b3982894bb2
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-23 18:37:44 +0530

    Time: 5 ms (91.57%), Space: 47.7 MB (97.25%) - LeetHub

--- 89-gray-code/89-gray-code.java
+++ 89-gray-code/89-gray-code.java
@@ -1,36 +1,31 @@
 class Solution {
-    
-    public List<String> helper(int n){
+    // approach 1 string
+    // app2 adding int
+    public List<Integer> helper(int n){
         if( n==1 ){
-            List<String> base = new ArrayList<>();
-            base.add("0");
-            base.add("1");
+            List<Integer> base = new ArrayList<>();
+            base.add(0);
+            base.add(1);
             return base;
         }
         
-        List<String> prev = helper(n-1);
+        List<Integer> prev = helper(n-1);
         
-        List<String> ans = new ArrayList<>();
-        for(int i=0 ; i<prev.size(); i++){
-            ans.add( "0" + prev.get(i) );
-        }
-        
+        // List<Integer> ans = new ArrayList<>();
+        // for(int i=0 ; i<prev.size(); i++){
+        //     ans.add( "0" + prev.get(i) );
+        // }
+        int one = 1<<(n-1);
         for(int i=prev.size()-1 ; i>=0 ; i--){
-            ans.add( "1" + prev.get(i) );
+            prev.add( one + prev.get(i) );
         }
-        return ans;
+        return prev;
     }
     
     public List<Integer> grayCode(int n) {
         
-        List<String> prev = helper(n);
+        return helper(n);
         
-        List<Integer> ans = new ArrayList<>();
         
-        for(String s : prev){
-            ans.add( Integer.parseInt(s,2) );
-        }
-        
-        return ans;
     }
 }
