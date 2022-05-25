ff9e2e6f69740e03f928a9901a6c6587ebc6fe61
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-25 16:26:51 +0530

    Time: 3 ms (80.82%), Space: 44.3 MB (49.01%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,33 +16,46 @@
 class Solution {
     
     HashMap<Integer, Integer> map ;
+    int preIdx;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         
         map = new HashMap<>();
-        
+        preIdx = 0;
         for(int i=0 ; i<inorder.length ; i++){
             map.put(inorder[i], i);
         }
         
-        TreeNode tree = build(0,preorder.length-1, 0,inorder.length-1, preorder);
+        TreeNode tree = build(0,preorder.length-1, preorder);
         return tree;
     }
     
-    private TreeNode build(int pl,int ph,int il,int ih, int[] pre){
+    private TreeNode build(int left,int right,int[] pre){
         
-        if( pl > ph || il > ih ){
+        if( right < left){
             return null;
         }
         
-        TreeNode node = new TreeNode( pre[pl] );
+        int val = pre[preIdx++];
+        TreeNode node = new TreeNode( val );
         
-        int idx = map.get( pre[pl] );
-        int lhs = idx - il;
-        
-        node.left = build(pl+1, pl+lhs , il, idx-1, pre);
-        node.right = build( pl+lhs+1, ph , idx+1, ih , pre);
+        node.left = build(left, map.get( val ) -1 , pre);
+        node.right = build( map.get(val) + 1 ,right,  pre);
         
         return node;
     }
-    
 }
+
+// app 1
+//         if( pl > ph || il > ih ){
+//             return null;
+//         }
+        
+//         TreeNode node = new TreeNode( pre[pl] );
+        
+//         int idx = map.get( pre[pl] );
+//         int lhs = idx - il; // no of elem in left side
+        
+//         node.left = build(pl+1, pl+lhs , il, idx-1, pre);
+//         node.right = build( pl+lhs+1, ph , idx+1, ih , pre);
+        
+//         return node;
