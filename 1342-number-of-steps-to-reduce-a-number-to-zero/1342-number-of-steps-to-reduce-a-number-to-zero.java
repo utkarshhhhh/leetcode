1e82107f680fc382c4a57b6e9204d86255eed2bb
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-27 16:15:39 +0530

    Time: 1 ms (54.47%), Space: 41.4 MB (18.39%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -3,6 +3,7 @@ class Solution {
         
         if( num == 0 ) return 0;
         
-        return (num%2==0 ? numberOfSteps(num>>1)  : numberOfSteps(num-1)) + 1;
+        // return (num%2==0 ? numberOfSteps(num>>1)  : numberOfSteps(num-1)) + 1;
+        return Integer.toBinaryString(num).length() -1 + Integer.bitCount(num);
     }
 }
