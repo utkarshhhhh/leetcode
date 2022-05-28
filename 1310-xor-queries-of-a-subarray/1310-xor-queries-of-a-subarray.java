5016e3e8ae8a634bc519e36bfa34746c0564e2bf
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-28 12:34:45 +0530

    Time: 26 ms (21.97%), Space: 70.3 MB (5.10%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(-1,0);
+        int xor = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            xor = xor^arr[i];
+            map.put( i, xor );
+        }
+        
+        int[] ans = new int[queries.length];
+        
+        for(int i=0 ; i<queries.length ; i++){
+            int[] ar = queries[i];
+            int l = ar[0], r = ar[1];
+            
+            ans[i] = map.get(l)^map.get(r)^arr[l];
+            
+        }
+        return ans;
+    }
+}
