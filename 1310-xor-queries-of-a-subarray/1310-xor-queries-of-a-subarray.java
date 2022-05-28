51e26a81c49d121d55070be9a2318fdbd3011e6f
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-28 12:36:08 +0530

    Time: 17 ms (23.89%), Space: 75.6 MB (5.10%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -10,9 +10,9 @@ class Solution {
         }
         
         int[] ans = new int[queries.length];
-        
+        int[] ar;
         for(int i=0 ; i<queries.length ; i++){
-            int[] ar = queries[i];
+            ar = queries[i];
             int l = ar[0], r = ar[1];
             
             ans[i] = map.get(l)^map.get(r)^arr[l];
