719b69ef4636f64413af51df5eba6f69ff570cf1
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-28 17:05:48 +0530

    Time: 1 ms (65.63%), Space: 47.1 MB (56.13%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -23,25 +23,50 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+    
+        Node p1 = root;
         
-        if(root == null || (root.left == null && root.right == null) ){ 
-            return root;
-        }
-        
-        
-        Node left = connect(root.left);
-        Node right = connect(root.right);
-        
-        left.next = right;
-        
-        while( left.right !=null  ){            
-            left.right.next = right.left;            
+        while( p1!=null ){
             
-            left = left.right;
-            right = right.left;
+            Node p2 = p1;
+            
+            while( p2!=null ){
+                
+                if( p2.left != null ){
+                    p2.left.next = p2.right;
+                }
+                
+                if( p2.right!=null && p2.next != null ){
+                    p2.right.next = p2.next.left;
+                }
+                p2 = p2.next;
+            }
+            
+            
+            p1 = p1.left;
         }
         
         
         return root;
     }
 }
+
+//         if(root == null || (root.left == null && root.right == null) ){ 
+//             return root;
+//         }
+        
+        
+//         Node left = connect(root.left);
+//         Node right = connect(root.right);
+        
+//         left.next = right;
+        
+//         while( left.right !=null  ){            
+//             left.right.next = right.left;            
+            
+//             left = left.right;
+//             right = right.left;
+//         }
+        
+        
+//         return root;
