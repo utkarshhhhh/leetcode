5bc41af85c2a3beddcd013e116990a04a01f9e05
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-29 19:27:59 +0530

    Time: 1 ms (91.68%), Space: 44.2 MB (42.30%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -21,7 +21,7 @@ class Solution {
         int x1 = 0, x2 = 0, x3 = 0;
         
         for (int i = 0; i < nums.length; i ++) {
-            x3 ^= x2 & nums[i];
+            // x3 ^= x2 & nums[i];
             x2 ^= x1 & nums[i];
             x1 ^= (~x2) & nums[i];
         }
