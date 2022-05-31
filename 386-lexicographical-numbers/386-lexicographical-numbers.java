32922d02752e38a50ca17c475dd3366e017b2554
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-31 18:30:55 +0530

    Time: 12 ms (24.42%), Space: 56.9 MB (41.86%) - LeetHub

---
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    static List<Integer> ans ;
+    public static void solve(int i, int n){
+        
+        if( i>n ){
+            return ;
+        }
+        
+        // List<Integer> ans = new ArrayList<>();
+        
+        // System.out.println(i);
+        ans.add(i);
+        for(int j=0 ; j<=9 ; j++){
+            solve( i*10+j, n);
+        }
+        // return ans;
+    }
+    
+    public List<Integer> lexicalOrder(int n) {
+        
+        ans = new ArrayList<>();
+        for(int i=1 ; i<=9 ; i++){
+		    solve(i,n );
+		}
+        return ans;
+    }
+}
