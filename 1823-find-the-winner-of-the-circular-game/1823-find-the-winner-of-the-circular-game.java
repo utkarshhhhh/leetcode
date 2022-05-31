b683c797cb6e63ba67624fc8519b40dd99e914d1
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-31 18:49:20 +0530

    Time: 2 ms (70.76%), Space: 40.1 MB (79.80%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,15 +1,10 @@
 class Solution {
     
-//     private int josepheus(int n, int k){
-        
-//     }
-    
     public int findTheWinner(int n, int k) {
         
-        if( n==1 ) return 1;
-        
-        int nm1 = findTheWinner(n-1, k);
-    
-        return  (nm1+k-1)%n+1 ;
+        if( n==1 ) return 1;        
+        int nm1 = findTheWinner(n-1, k);    
+        // subtract 1 for 0 based indexing , add for 1 based indexing
+        return  (nm1+k-1)%n +1 ;        
     }
 }
