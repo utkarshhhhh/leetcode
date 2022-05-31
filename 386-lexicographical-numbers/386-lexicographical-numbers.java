56dc4169974dd9e162d7c0d4b9a84e654138b994
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-05-31 18:32:05 +0530

    Time: 5 ms (74.42%), Space: 57.4 MB (15.58%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -11,6 +11,7 @@ class Solution {
         // System.out.println(i);
         ans.add(i);
         for(int j=0 ; j<=9 ; j++){
+            if( i*10+j > n ) return;
             solve( i*10+j, n);
         }
         // return ans;
