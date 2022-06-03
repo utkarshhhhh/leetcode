a90caea9c637e7fc09f5681105e500bd06fb8b7d
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-03 10:47:01 +0530

    Time: 206 ms (33.56%), Space: 126.3 MB (62.55%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -3,39 +3,16 @@ class NumMatrix {
     int[][] preSum;
     public NumMatrix(int[][] matrix) {
         
-        for(int i=0 ; i<matrix.length ; i++){
-            for(int j=0 ; j<matrix[0].length ; j++){
-                
-                if( i==0 && j==0 ){
-                    continue;
-                }else if( i==0 ){
-                    matrix[i][j] += matrix[i][j-1];
-                }else if( j==0 ){
-                    matrix[i][j] += matrix[i-1][j];
-                }else{
-                    matrix[i][j] += matrix[i-1][j] + matrix[i][j-1] - matrix[i-1][j-1];
-                }                
-            }
-        }
-        preSum = matrix;
+        preSum = new int[matrix.length+1][matrix[0].length+1];
+        for(int i=1 ; i<=matrix.length ; i++){
+            for(int j=1 ; j<=matrix[0].length ; j++)                
+                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1] + matrix[i-1][j-1];               
+        }        
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
         
-        int sum = preSum[row2][col2];
-        
-        if( row1 != 0 ){
-            sum -= preSum[row1-1][col2];
-        }
-        
-        if( col1 != 0 ){
-            sum -= preSum[row2][col1-1];
-        }
-        
-        if( row1 != 0 && col1 != 0 ){
-            sum += preSum[row1-1][col1-1];
-        }
-        return sum;
+        return preSum[row2+1][col2+1] - preSum[row1][col2+1] - preSum[row2+1][col1] + preSum[row1][col1];        
     }
 }
 
