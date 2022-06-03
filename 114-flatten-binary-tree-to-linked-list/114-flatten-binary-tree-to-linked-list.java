740b4ce887810ac486a328c82ee07ccd1b6b4488
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-03 14:19:03 +0530

    Time: 1 ms (66.66%), Space: 42.5 MB (57.76%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -17,31 +17,74 @@ class Solution {
     
     class pair{
         TreeNode head, tail;
+        pair(){}
+        pair( TreeNode h, TreeNode t ){
+            head = h;
+            tail = t;
+        }
     }
     
-    TreeNode prev = null;
     
-//     private TreeNode helper(TreeNode root){
+    private pair helper(TreeNode root){
         
-//         if( root==null ){
-//             return null;
-//         }
-//         TreeNode left = flatten(root.left);
-//         TreeNode right = flatten(root.right);
-//         root.left = null;
-//         left.right = 
-//     }
+        if( root.left != null && root.right != null ){
+            pair left = helper(root.left);
+            pair right = helper(root.right);
+
+            pair node = new pair();
+            node.head = root;
+            node.head.left = null;
+            node.head.right = left.head;
+            left.tail.right = right.head;
+            node.tail = right.tail;
+            
+            return node;
+        }else if( root.left != null ){
+            pair left = helper(root.left);
+            
+            pair node = new pair();
+            node.head = root;
+            node.head.left = null;
+            node.head.right = left.head;
+            node.tail = left.tail;
+
+            return node;                      
+        }else if( root.right != null ){
+            pair right = helper(root.right);
+            
+            pair node = new pair();
+            node.head = root;
+            node.head.left = null;
+            node.head.right = right.head;
+            node.tail = right.tail;
+            
+            return node;
+        }else{
+            return new pair(root,root);
+        }
+       
+    }
     
     public void flatten(TreeNode root) {
         
-        if( root==null ){
-            return;
-        }
-        
-        flatten(root.right);
-        flatten(root.left);
-        root.left = null;
-        root.right = prev;
-        prev = root;
+        if(root != null){
+            helper(root);
+        }        
     }
+    
+    
 }
+
+/*
+    TreeNode prev = null;
+
+if( root==null ){
+        return;
+    }
+
+    flatten(root.right);
+    flatten(root.left);
+    root.left = null;
+    root.right = prev;
+    prev = root;
+*/
