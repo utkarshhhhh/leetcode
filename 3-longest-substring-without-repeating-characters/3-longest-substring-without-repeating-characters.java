781d49292e6fd4e2792028efc4e886fdabf7db76
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-10 21:27:22 +0530

    Time: 32 ms (20.40%), Space: 45.8 MB (34.47%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -3,20 +3,34 @@ class Solution {
         
         if(s.length() == 0 ) return 0;
         
-        HashMap<Character, Integer> hash = new HashMap<>();
-        
         int ans = 0;
-        for( int i=0 , j = 0; i<s.length()  ; i++){
+//         HashMap<Character, Integer> hash = new HashMap<>();
+        
+//         for( int i=0 , j = 0; i<s.length()  ; i++){
             
-            char c = s.charAt(i);
+//             char c = s.charAt(i);
             
-            if( hash.containsKey(c) ){
-                j = Math.max( hash.get(c) + 1 ,j);
-            }   
+//             if( hash.containsKey(c) ){
+//                 j = Math.max( hash.get(c) + 1 ,j);
+//             }   
             
-            hash.put( c, i );            
-            ans = Math.max( ans, i-j + 1 );            
+//             hash.put( c, i );            
+//             ans = Math.max( ans, i-j + 1 );            
+//         }
+        
+        Queue<Character> q = new LinkedList<>();
+        
+        for(char c : s.toCharArray() ){
+            
+            while( q.contains(c) ){
+                q.poll();
+            }
+            
+            q.add(c);
+            ans = Math.max(ans, q.size() );
         }
+        
+        
         return ans;
     }
 }
