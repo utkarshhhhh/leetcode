d22c2fc02e5f271f313511b1f459a577f503f7a5
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-14 17:26:10 +0530

    Time: 3 ms (68.71%), Space: 42.9 MB (52.48%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -9,19 +9,18 @@ class Solution {
     
     private int count(int[][] grid ,Queue<pair> q){
         
-        int n = grid.length , m = grid[0].length;
-        int c = 0;
-        for(int i=0 ; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                
-                if( grid[i][j] == 2 ){
-                    q.add( new pair(i,j) );
-                }else if( grid[i][j] == 1 ){
-                    c++;
+        int fresh = 0;
+        int n = grid.length, m = grid[0].length;
+        for(int r=0 ; r<n ; r++){
+            for(int c=0 ; c<m ; c++){
+                if( grid[r][c] == 1 ){
+                    fresh++;
+                }else if( grid[r][c] == 2 ){
+                    q.add( new pair(r,c) );
                 }
             }
         }
-        return c;
+        return fresh;
     }
     
     private int[] dir = { 0, 1, 0, -1, 0 };
@@ -30,44 +29,42 @@ class Solution {
         
         int n = grid.length , m = grid[0].length;
         int time = 0;
-        Queue<pair> q = new LinkedList<>();
-        int fresh = count(grid , q);
         
-        if( fresh == 0 )
-            return time;          
+        Queue<pair> q =new LinkedList(); // places of rottenO.
+
+        int fresh = count( grid, q );
+        
+        if(fresh == 0) return 0;
+        
         while( !q.isEmpty() ){
             
             int size = q.size();
             
-            while( size-- > 0 ){
+            for(int i=0; i<size ; i++){
+            
+                pair cur = q.poll();
+                int x = cur.i, y = cur.j;
+
+                // if( grid[x][y] != 2 ) continue;
+
                 
-                pair rem = q.remove();
-                
-                // if (grid[rem.i][rem.j] == 1 ){
-                //     grid[rem.i][rem.j] = 2;
-                //     fresh--;
-                // }
-                // if( fresh == 0 ){
-                //     return time;
-                // }
-                
-                for(int i=0 ; i<4 ; i++ ){
-                    int r = rem.i + dir[i];
-                    int c = rem.j + dir[i+1];
+                for( int d=0 ; d<4 ; d++ ){
                     
-                    if( r>=0 && r<n && c>=0 && c<m && grid[r][c] == 1 ){
-                        q.add( new pair(r,c) ); 
+                    int r = x + dir[d], c = y + dir[d+1];
+                    if( r>=0 && c>=0 && r<n && c<m && grid[r][c] == 1 ){
                         
-                       
-                            grid[r][c] = 2;
-                            fresh--;                        
-                        if( fresh == 0 )
-                            return time+1;                                         
-                    }
-                }                
+                        grid[r][c] = 2;
+                        fresh--;
+                        
+                        if(fresh==0) return time+1;
+                        
+                        q.add( new pair(r,c) );
+                    }                    
+                }
             }
+            
             time++;
         }
-        return fresh!=0 ? -1 : time;
+        return fresh==0?time : -1;
     }
 }
