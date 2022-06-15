a563f90d283983779b70f153c924a5506c70e835
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-15 10:47:52 +0530

    Time: 77 ms (41.86%), Space: 44.5 MB (43.35%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -5,15 +5,13 @@ class Solution {
         int[] dp = new int[n];
         int ans = 0;
         
-        dp[0] = 1;
+        Arrays.fill(dp,1);
         
         for(int i=1 ; i<n ; i++){
             for(int j=0 ; j<i ; j++){
                 if( nums[i] > nums[j]  ){
                     dp[i] = Math.max(dp[j] + 1, dp[i] );
                 }
-                else
-                    dp[i] = Math.max(1, dp[i] );
             }
         }
         
