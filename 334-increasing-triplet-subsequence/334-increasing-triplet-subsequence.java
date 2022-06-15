ac1569a0a6470a1bda9b16566c34005e4b0b333e
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-15 11:17:19 +0530

    Time: 6 ms (21.59%), Space: 93.6 MB (47.24%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -1,22 +1,34 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         
-        Integer x = null, y = null, z = null;
+        Integer sm = Integer.MAX_VALUE, lg = Integer.MAX_VALUE;
+        
+//         for(int i : nums){
+            
+//             if( x == null || x >= i ){
+//                 if( x == null ) x = i;
+//                 else x = Math.min(x,i);
+//             }else if( y == null || y >= i ){
+//                 if( y == null ) y = i;
+//                 else y = Math.min(y,i);
+//             }else if( z == null || z >= i ){
+//                 if( z == null ) z = i;
+//                 else z = Math.min(z,i);
+//             }
+            
+//         }
+//         return x != null && y != null && z != null;
         
         for(int i : nums){
             
-            if( x == null || x >= i ){
-                if( x == null ) x = i;
-                else x = Math.min(x,i);
-            }else if( y == null || y >= i ){
-                if( y == null ) y = i;
-                else y = Math.min(y,i);
-            }else if( z == null || z >= i ){
-                if( z == null ) z = i;
-                else z = Math.min(z,i);
-            }
+            if( sm >= i ){
+                sm = i;
+            }else if( lg >= i ){
+                lg = i;
+            }else
+                return true;
             
         }
-        return x != null && y != null && z != null;
+        return false;
     }
 }
