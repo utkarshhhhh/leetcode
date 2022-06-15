af823aada0b077e9c48fef63f56fa66829b53fe9
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-15 11:14:11 +0530

    Time: 10 ms (11.51%), Space: 94.8 MB (18.70%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        
+        Integer x = null, y = null, z = null;
+        
+        for(int i : nums){
+            
+            if( x == null || x >= i ){
+                if( x == null ) x = i;
+                else x = Math.min(x,i);
+            }else if( y == null || y >= i ){
+                if( y == null ) y = i;
+                else y = Math.min(y,i);
+            }else if( z == null || z >= i ){
+                if( z == null ) z = i;
+                else z = Math.min(z,i);
+            }
+            
+        }
+        return x != null && y != null && z != null;
+    }
+}
