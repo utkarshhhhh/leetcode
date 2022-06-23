f75055978fa2a645f559bd44cbaf21d79b4234b2
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-23 17:47:26 +0530

    Time: 6 ms (67.52%), Space: 42.6 MB (43.67%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -2,19 +2,20 @@ class Solution {
     public int countSubstrings(String s) {
         
         int ans = 0;
-        for( int i=0 ; i<s.length() ; i++ ){            
-            ans += pallindrome( s, i ,i ) + pallindrome( s, i ,i+1 );            
+        for(int i=0 ; i<s.length() ; i++){
+            ans += palindrome( s, i,i ) + palindrome( s, i,i+1 ) ;
         }
         return ans;
     }
     
-    int pallindrome( String s, int i, int j ){
+    public int palindrome(String s, int i, int j){
         
         int ans = 0;
         while( i>=0 && j<s.length() && s.charAt(i) == s.charAt(j) ){
-            i--; j++; ans++;
+            i--;
+            j++;
+            ans++;
         }
         return ans;
     }
-    
 }
