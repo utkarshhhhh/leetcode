30769ae4e6b3581121d8731ffefb0d0390956ada
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-24 15:54:47 +0530

    Time: 5 ms (55.58%), Space: 42.5 MB (71.36%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -31,7 +31,26 @@ class Solution {
     
     public boolean isMatch(String s, String p) {
      
-        return helper( s,p, 0 , 0, new Boolean[s.length()+1][p.length()] );
+        // return helper( s,p, 0 , 0, new Boolean[s.length()+1][p.length()] );
         // return s.matches(p);
+        
+        int pl = p.length(), sl = s.length();
+        boolean[][] dp = new boolean[sl+1][pl+1];
+        
+        dp[0][0] = true;
+        for(int j=2 ; j<=pl ; j++){
+            dp[0][j] = p.charAt(j-1) == '*' && dp[0][j-2]; 
+ 
+        }
+        
+        for( int i=1 ; i<=sl ; i++ ){
+            for(int j=1 ; j<=pl ; j++)
+           
+                if( s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1) == '.' )
+                    dp[i][j] = dp[i-1][j-1];
+                else if( p.charAt(j-1) == '*') 
+                    dp[i][j] = dp[i][j-2] || ((s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') && dp[i-1][j]); 
+        }
+        return dp[sl][pl];
     }
 }
