bffd44a2d74e0191b27679ac65d8eff16b5efb89
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-24 15:00:10 +0530

    Time: 82 ms (49.92%), Space: 48.1 MB (53.70%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -33,14 +33,15 @@ class Solution {
         return dp[l][r] = min;
     }
     
+    
     public int minCut(String s) {
      
         int n = s.length();
-        int[][] dp = new int[n][n];
+        // int[][] dp = new int[n][n];
         
-        for(int[] d : dp){
-            Arrays.fill(d, -1);
-        }
+        // for(int[] d : dp){
+        //     Arrays.fill(d, -1);
+        // }
         
         boolean[][] pal = new boolean[n][n];
         
@@ -51,19 +52,78 @@ class Solution {
             }            
         }
         
-        return helper(s, 0, s.length()-1, dp, pal);
+        // return helper(s, 0, s.length()-1, dp, pal);
         
-//         for(int left = n-1; left>=0 ; left--){
-//             for( int right = left+2 ; right<n ; right++ ){
-                
-//                 if(pal[left][right]){
-//                     dp[left][right] = 0;
-//                 }else{
-//                     dp[left][right] = Math.min( dp[left+1][right], dp[left][right-1] ) + 1;
-//                 }
-//             }            
-//         }
         
-//         return dp[0][n-1];
+//         for (int i = 0, j = 1; j < s.length(); i++, j++)
+// 	    	dp[i][j] = (s.charAt(i) == s.charAt(j)) ? 0 : 1;
+    
+        int[] dp = new int[n];
+        
+        for(int r = 1 ; r<n ; r++){
+            
+            if( pal[0][r] ) continue;
+            
+            int min = 5000;
+            for( int l = r ; l>=1 ; l-- ){                
+                if( pal[l][r] ){                    
+                    min = Math.min( min, 1 + dp[l-1] );                    
+                }                
+            }
+            dp[r] = min;
+        }
+        
+        return dp[n-1];
     }
 }
+
+
+
+/*
+n3
+ 
+int n = s.length();
+        int[][] dp = new int[n][n];
+        
+        // for(int[] d : dp){
+        //     Arrays.fill(d, -1);
+        // }
+        
+        boolean[][] pal = new boolean[n][n];
+        
+        for(int left = n-1; left>=0 ; left--){
+            pal[left][left] = true;
+            // dp[left][left] = 1;
+            for( int right = left+1 ; right<n ; right++ ){
+                pal[left][right] = s.charAt(left)==s.charAt(right) && ( left+1==right || pal[left+1][right-1] );
+            }            
+        }
+        
+        // return helper(s, 0, s.length()-1, dp, pal);
+        
+        
+        for (int i = 0, j = 1; j < s.length(); i++, j++)
+	    	dp[i][j] = (s.charAt(i) == s.charAt(j)) ? 0 : 1;
+    
+        
+        for(int gap = 2 ; gap<n ; gap++){
+            for(int left = 0, right = gap; right<n ; left++, right++){
+            
+                if(pal[left][right]){
+                    // dp[i][right] = 0;
+                    continue;
+                }
+                
+                int min = 5000;
+                for( int i = left ; i<right ; i++ ){
+                
+                 min =  Math.min( dp[left][i] + dp[i+1][right] + 1 , min );
+                }
+                dp[left][right] = min;
+            }            
+        }
+        
+        return dp[0][n-1];
+    
+
+*/
