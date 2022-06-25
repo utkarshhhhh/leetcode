32b128561f11244b6e9664d75cc11dbe90adddcd
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-06-25 18:19:12 +0530

    Time: 1 ms (97.53%), Space: 53.7 MB (65.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -2,21 +2,37 @@ class Solution {
     public boolean checkPossibility(int[] nums) {
         
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, 1);
         
+//         for(int i=1 ; i<n ; i++){
+//             for(int j=0 ; j<i ; j++){
+//                 if( nums[i] >= nums[j] ){
+//                     dp[i] = Math.max( dp[i], dp[j]+1 );
+//                 }
+//             }
+//         }
+        
+//         int max = 0;
+        
+//         for(int x : dp) max = Math.max(x, max);
+        
+//         return max+1 >= n;
+        
+        int count = 0;
         for(int i=1 ; i<n ; i++){
-            for(int j=0 ; j<i ; j++){
-                if( nums[i] >= nums[j] ){
-                    dp[i] = Math.max( dp[i], dp[j]+1 );
+            
+            if( nums[i] < nums[i-1] ){
+                
+                if(count > 0) return false;
+                count++;
+                
+                if( i-2>=0 && ( nums[i-2] > nums[i] ) ){
+                    nums[i] = nums[i-1];
                 }
             }
         }
         
-        int max = 0;
-        
-        for(int x : dp) max = Math.max(x, max);
-        
-        return max+1 >= n;
+        return true;
     }
 }
