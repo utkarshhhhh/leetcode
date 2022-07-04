e8782e2a31a3afbff0ab44dfac485b688ffc0363
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-04 15:44:09 +0530

    Added solution - LeetHub

---
+++ Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.java
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.longestSubstrDistinctChars(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int longestSubstrDistinctChars(String S){
+        // code here
+        
+        HashSet<Character> set = new HashSet<>();
+        int ans = 0, cur=0;
+        int i=0 , j = 0;
+        while( j<S.length() ){
+            
+            char c = S.charAt(j);
+            
+            while( set.contains(c) && i<j){
+                set.remove( S.charAt(i++) );
+            }
+            
+            set.add(c);
+            ans = Math.max(ans, set.size() );
+            j++;
+        }
+        return ans;
+    }
+}
