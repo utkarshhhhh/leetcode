7da164adcdda8089b8cdc2b98eda74203cdc3e3f
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-06 16:40:54 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (28.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,12 @@
 class Solution {
     public int fib(int n) {
         if( n <= 1 ) return n;
-        return fib(n-1)+fib(n-2);
+        int x = 0, y = 1, z = 0;
+        for(int i=2 ; i<=n ; i++){
+            z = x+y;
+            x = y;
+            y = z;
+        }
+        return z;
     }
 }
