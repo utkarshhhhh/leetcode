996b92657f33f7a15798297396c3c8a0498555f4
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-07 13:06:09 +0530

    Time: 17 ms (13.02%), Space: 42.1 MB (70.78%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -27,6 +27,28 @@ class Solution {
         return dp[i][j] = false;
     }
     
+    public boolean tab2D(String s1, String s2, String s3) {
+        
+        
+        for(int i=0 ; i<=s1.length() ; i++){
+            for(int j=0 ; j<=s2.length() ; j++){
+            
+                if( i==0 && j==0 ){
+                    dp[i][j] = true;
+                }else if( i==0 ){
+                    dp[i][j] = s2.charAt(j-1) == s3.charAt(i+j-1) && dp[i][j-1] ;
+                }else if( j==0 ){
+                    dp[i][j] = s1.charAt(i-1) == s3.charAt(i+j-1) && dp[i-1][j] ;
+                }else{
+                    
+                    dp[i][j] = (s1.charAt(i-1) == s3.charAt(i+j-1) && dp[i-1][j]) || (s2.charAt(j-1) == s3.charAt(i+j-1) && dp[i][j-1]) ;                    
+                }
+                
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+    
     public boolean isInterleave(String s1, String s2, String s3) {
         
         if( s1.length() + s2.length() != s3.length() ){
@@ -35,7 +57,7 @@ class Solution {
         
         dp = new Boolean[s1.length()+1][s2.length()+1];
         
-        return helper(s1,s2,s3,0,0,0);
-        
+        // return helper(s1,s2,s3,0,0,0);
+        return tab2D(s1,s2,s3);
     }
 }
