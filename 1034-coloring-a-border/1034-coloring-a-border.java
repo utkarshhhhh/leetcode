7af6766fca7235c23d29393806285f4e626904f4
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-14 23:11:39 +0530

    Time: 1 ms (98.54%), Space: 54.5 MB (41.51%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -6,85 +6,50 @@ class Solution {
         Pair( int i, int j){
             x = i;
             y = j;
-        }
-        
-    }
-       
-    void bfs(int[][] grid, int r, int c, int x, boolean[][] vis,Queue<Pair> q ){
-        
-        if( r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c] != x || vis[r][c] ){
-            return ;
-        }
-        
-        vis[r][c] = true;
-        q.add( new Pair( r,c ) );
-        
+        }        
     }
     
-    void dfs(int[][] grid, int r, int c, int x){
+    public void dfs( int[][] grid, int r, int c, int org ){
         
-        if( r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c] != x ){
+        int n = grid.length , m = grid[0].length ;
+        
+        if( r<0 || r>=n || c<0 || c>=m || grid[r][c] != org ){
             return ;
         }
         
         grid[r][c] = -grid[r][c];
         
-        dfs( grid, r+1, c, x);
-        dfs( grid, r-1, c, x);
-        dfs( grid, r, c+1, x);
-        dfs( grid, r, c-1, x);
-
-        fill( grid, r, c, x );
+        dfs( grid, r+1, c, org );
+        dfs( grid, r-1, c, org );
+        dfs( grid, r, c+1, org );
+        dfs( grid, r, c-1, org );
         
+        fillColor( grid, r, c, org );
     }
     
-    void fill(int[][] grid, int r, int c,int x){
-    
-        int n = grid.length, m = grid[0].length;
+    public void fillColor(int[][] grid, int r, int c, int color){
 
-        boolean left = c>0 ? Math.abs(grid[r][c-1])==x : false ; 
-        boolean right = c<m-1 ? Math.abs(grid[r][c+1])==x  : false ; 
-        boolean up = r>0 ? Math.abs(grid[r-1][c])==x  : false ; 
-        boolean down = r<n-1 ? Math.abs(grid[r+1][c])==x  : false ; 
-    
-        if( left && right && up && down ){
-            grid[r][c] = Math.abs(grid[r][c]);
-        }
+        int n = grid.length , m = grid[0].length ;
+        // check if ander ka h 
+        boolean left = r>0 ? Math.abs(grid[r-1][c]) == color : false;
+        boolean right = r<n-1 ? Math.abs(grid[r+1][c]) == color : false;
+        boolean up = c>0 ? Math.abs(grid[r][c-1]) == color : false;
+        boolean down = c<m-1 ? Math.abs(grid[r][c+1]) == color : false;
         
+        if( (left&&right&&up&&down) ){
+            grid[r][c] = color;
+        }
     }
     
     public int[][] colorBorder(int[][] grid, int row, int col, int color) {
-        
-        // Queue<Pair> q = new LinkedList<>();
-        int n = grid.length, m = grid[0].length;
     
-//         boolean[][] vis = new boolean[n][m];
-        
         int org = grid[row][col];
-//         q.add(new Pair( row,col ));
-//         vis[row][col] = true;
         
+        dfs( grid, row, col, org );
         
-//         while( !q.isEmpty() ){
-//             Pair cur = q.poll();
-//             int r = cur.x, c = cur.y;
-//             if( grid[r][c] != org ){
-//                 continue;
-//             }
-            
-//             bfs( grid, r+1, c, org, vis, q );
-//             bfs( grid, r-1, c, org, vis, q );
-//             bfs( grid, r, c+1, org, vis, q );
-//             bfs( grid, r, c-1, org, vis, q );
-            
-//             fill( grid,r,c, vis, color );
-//         }
-        
-        dfs(grid, row, col, org);
-        
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<m ; j++){
-                if( grid[i][j]<0 ) grid[i][j] = color;
+        for(int i=0 ; i<grid.length ; i++){
+            for(int j=0 ; j<grid[0].length ; j++){
+                if( grid[i][j] < 0 )    grid[i][j] = color;
             }
         }
         
