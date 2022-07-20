5e80b0ead5547a0670632a09623e1ff8ae63e50c
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-20 16:34:36 +0530

    Time: 75 ms (91.66%), Space: 43.3 MB (95.83%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,34 +1,34 @@
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
         
-        HashMap< Character, Deque<String> > map = new HashMap<>();
+        HashMap< Character, Deque<StringBuilder> > map = new HashMap<>();
         
         for(char c='a' ; c<='z' ; c++){
-            map.put( c, new ArrayDeque<String>() );
+            map.put( c, new ArrayDeque<StringBuilder>() );
         }
         
         for( String x : words ){
-            map.get( x.charAt(0) ).add( new String(x) );
+            map.get( x.charAt(0) ).add( new StringBuilder(x) );
         }
         
         int ans = 0;
         for( int i=0 ; i<s.length() ; i++ ){
             char c = s.charAt(i);
             
-            Deque<String> q = map.get( c );
+            Deque<StringBuilder> q = map.get( c );
             int size = q.size();
             for(int j=0 ; j<size  ; j++){
                 
-                String cur = q.poll() ;
-                // cur.deleteCharAt(0);
+                StringBuilder cur = q.poll() ;
+                cur.deleteCharAt(0);
                 
-                if( cur.length() == 1 ){ 
+                if( cur.length() == 0 ){ 
                     ans++ ;                   
                 }else{
-                    map.get( cur.charAt(1) ).addLast( cur.substring(1) );
+                    map.get( cur.charAt(0) ).add( cur );
                 }
-                
             }
+            
         }
         return ans;
     }
