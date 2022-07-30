a487094c5ab4db0f64119d70d433157590870a25
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-30 13:43:44 +0530

    Time: 2 ms (88.62%), Space: 58.4 MB (8.72%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -3,19 +3,18 @@ class Solution {
      
         int x = 1;
         int[] ans = new int[nums.length];
-        // ans[0] = 1;
-        for(int i=0 ; i<nums.length ; i++){
-            
+
+        for(int i=0 ; i<nums.length ; i++){            
             ans[i] = x;
             x = x*nums[i];
         }
         
         x = 1;
-        for(int i=nums.length-2 ; i>=0 ; i--){
-            
-            x = x*nums[i+1];
+        for(int i=nums.length-1 ; i>=0 ; i--){                        
             ans[i] = ans[i]*x;
+            x = x*nums[i];
         }
+        
         return ans;
     }
 }
