ae200740aa1d10f2128c276a4db1a08cfd72779d
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-30 13:54:03 +0530

    Time: 7 ms (31.60%), Space: 47.3 MB (5.56%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,24 +1,22 @@
 class Solution {
-    
-    private long solve(int[] arr, int x){
-        
-        long ans = 0;
-        
-        for(int i : arr){
-            ans += Math.abs( x-i );
-        }
-        return ans;
-    }
     public int minMoves2(int[] nums) {
         
-        Arrays.sort( nums );
-        long ans = 0;
-        int n = nums.length;
-        if( n%2 == 1 ){
-            return (int)solve( nums, nums[n/2] );
+        Arrays.sort(nums);
+        
+        int n = nums.length, median = 0;
+        
+        if( n%2 == 0 ){
+            median = (nums[n/2] + nums[n/2 - 1])/2;
         }else{
-            return (int) Math.max( solve(nums, nums[n/2]), solve(nums, nums[(n/2) - 1]) );
+            median = nums[n/2];
         }
         
+        int ans = 0;
+        
+        for( int i=0 ; i<n ; i++ ){
+            ans += Math.abs( nums[i] - median );
+        }
+        
+        return ans;
     }
 }
