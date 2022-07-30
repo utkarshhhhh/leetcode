2b58f0dfdee06d0bb38416b398d2634f2f94539b
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-30 13:56:33 +0530

    Time: 7 ms (31.60%), Space: 46.7 MB (8.60%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -5,16 +5,23 @@ class Solution {
         
         int n = nums.length, median = 0;
         
-        if( n%2 == 0 ){
-            median = (nums[n/2] + nums[n/2 - 1])/2;
-        }else{
-            median = nums[n/2];
-        }
+//         if( n%2 == 0 ){
+//             median = (nums[n/2] + nums[n/2 - 1])/2;
+//         }else{
+//             median = nums[n/2];
+//         }
+        
+//         int ans = 0;
+        
+//         for( int i=0 ; i<n ; i++ ){
+//             ans += Math.abs( nums[i] - median );
+//         }
+        
         
         int ans = 0;
         
-        for( int i=0 ; i<n ; i++ ){
-            ans += Math.abs( nums[i] - median );
+        for( int i=0, j=n-1 ; i<j ; i++, j-- ){
+            ans += Math.abs( nums[j] - nums[i] );
         }
         
         return ans;
