a9a5d5b81b8af084c7ec5a55baf84d43a308f305
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-07-31 14:54:30 +0530

    Time: 144 ms (55.87%), Space: 126.8 MB (70.28%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -0,0 +1,43 @@
+class NumArray {
+
+  int[] tree;
+    int n;
+
+    public NumArray(int[] nums) {
+        n = nums.length;
+        tree = new int[n << 1];
+        buildTree(nums);
+    }
+
+    private void buildTree(int[] nums) {
+        for (int i = n; i < n << 1; i++) {
+            tree[i] = nums[i - n];
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            tree[i] = tree[i << 1] + tree[i << 1 | 1];
+        }
+    }
+
+    void update(int i, int val) {
+        for (tree[i += n] = val; i > 0; i >>= 1) {
+            tree[i >> 1] = tree[i] + tree[i ^ 1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        int ret = 0;
+        for (i += n, j += n; i <= j; i >>= 1, j >>= 1) {
+            if ((i & 1) == 1) ret += tree[i++];
+            if ((j & 1) == 0) ret += tree[j--];
+        }
+        return ret;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
