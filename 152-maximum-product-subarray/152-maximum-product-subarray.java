2f3bc0a1da96ae7d509e3424c9d96f151a54075b
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-08-02 18:49:56 +0530

    Time: 2 ms (78.13%), Space: 44.9 MB (50.78%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -2,22 +2,42 @@ class Solution {
     public int maxProduct(int[] nums) {
         // prefix suffix approach
         
-        int ans = Integer.MIN_VALUE, mul = 1;
+//         int ans = Integer.MIN_VALUE, mul = 1;
         
-        for(int x : nums){
+//         for(int x : nums){
             
-            mul = mul*x;
-            ans = Math.max(mul, ans);
-            if( mul==0 ) mul = 1;
+//             mul = mul*x;
+//             ans = Math.max(mul, ans);
+//             if( mul==0 ) mul = 1;
+//         }
+        
+//         mul = 1;
+//         for(int i=nums.length-1 ; i>=0 ; i--){
+            
+//             mul = mul*nums[i];
+//             ans = Math.max(mul, ans);
+//             if( mul==0 ) mul = 1;
+//         }
+//         return ans;
+        
+        int min = 1, max = 1, ans = Integer.MIN_VALUE;
+        
+        for( int x : nums ){
+            
+            if( x>=0 ){
+                
+                max = Math.max( max*x, x );
+                min = Math.min( min*x, x );
+                
+            }else{
+                int temp = max;
+                max = Math.max( min*x, x );
+                min = Math.min( temp*x, x );
+                
+            }
+            ans = Math.max(ans, max);
         }
         
-        mul = 1;
-        for(int i=nums.length-1 ; i>=0 ; i--){
-            
-            mul = mul*nums[i];
-            ans = Math.max(mul, ans);
-            if( mul==0 ) mul = 1;
-        }
         return ans;
     }
 }
