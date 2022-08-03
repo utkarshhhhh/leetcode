95ce841908df4fabf62c5fad663a0123aed0b064
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-08-03 21:54:59 +0530

    Time: 1 ms (99.77%), Space: 49.3 MB (20.62%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,47 +1,25 @@
 class Solution {
     public int trap(int[] height) {
         
-        int n = height.length;
-//         int[] left = new int[n], right = new int[n];
+        //app1 : 2 arr max left right
         
-//         left[0] = height[0];
-//         right[n-1] = height[n-1];
+        int i=0 , j = height.length-1;
+        int left = 0, right = 0, ans = 0;
         
-//         for(int i=1 ; i<n; i++){
-//             left[i] = Math.max( left[i-1], height[i] );
-//         }
-        
-//         for(int i=n-2 ; i>=0; i--){
-//             right[i] = Math.max( right[i+1], height[i] );
-//         }
-        
-        int ans = 0;
-        
-//         for( int i=1 ; i<n-1 ; i++ ){        
-//             ans += Math.min( left[i], right[i] ) - height[i];            
-//         }
-        
-        int left = height[0], right = height[n-1];
-        
-        int i = 0, j = n-1;
-        while( i < j ){
+        while(i<=j){
             
-            left = Math.max(left, height[i]);
-            right = Math.max(right, height[j]);
+            left = Math.max(left, height[i]);    
+            right = Math.max(right, height[j]);    
             
-            if( left <= right ){
-                
-                ans += left - height[i];
+            if( left < right ){
+                ans += left-height[i];
                 i++;
-            
             }else{
-                
-                ans += right - height[j];
+                ans += right-height[j];
                 j--;
+            }
             
-            }            
         }
-        
         
         return ans;
     }
