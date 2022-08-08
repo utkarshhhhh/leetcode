aa712dc69fc5e8e0dc17de8424f0a1107454a477
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-08-08 17:06:35 +0530

    Time: 80 ms (48.42%), Space: 44.7 MB (32.42%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -2,39 +2,20 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         
         int n = nums.length;
-        int[] dp = new int[n];
-        int ans = 0;
+        int[] hash = new int[n];
         
-        // Arrays.fill(dp,1);
-        
-        // for(int i=1 ; i<n ; i++){
-        //     for(int j=0 ; j<i ; j++){
-        //         if( nums[i] > nums[j]  ){
-        //             dp[i] = Math.max(dp[j] + 1, dp[i] );
-        //         }
-        //     }
-        //      ans = Math.max(ans, dp[i]);
-        // }
-        
-        for(int x : nums){
-            
-            int l = 0, h = ans;
-            
-            while( l < h ){
+        Arrays.fill(hash, 1);
+        int ans = 1;
+        for(int i=1 ; i<n ; i++){
+            for(int j=0 ; j<i ; j++){
                 
-                int m = l + (h-l)/2;
-                if( dp[m] >= x ){
-                    h = m;
-                }else{
-                    l = m+1;
-                }    
+                if( nums[i] > nums[j] ){
+                    hash[i] = Math.max( hash[i], hash[j]+1 );
+                }
+                
             }
-            
-            dp[l] = x;
-            if( l==ans ) ans++;
-            
+            ans = Math.max(ans, hash[i]);
         }
-        
         return ans;
     }
 }
