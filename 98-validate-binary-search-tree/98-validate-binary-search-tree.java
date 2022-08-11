eabf64739deb646bce25527ef73933a0b3832301
Utkarsh Srivastava <74353439+utkarshhhhh@users.noreply.github.com>
2022-08-11 11:47:55 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (86.99%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,27 +15,21 @@
  */
 class Solution {
     
-    
-    private long prev;
-    public boolean inorder(TreeNode root){
-        if(root == null) return true ;
-        
-        boolean left = inorder(root.left);
-        if( !left ) return false;
-        
-        if( root.val <= prev )
-            return false;
-        prev = root.val;
-        
-        boolean right = inorder(root.right);
-        if( !right ) return false;
-        
-        return true;
-    }
+    long prev = Long.MIN_VALUE;
     
     public boolean isValidBST(TreeNode root) {
         
-        prev = Long.MIN_VALUE;
-        return inorder(root);
+        if( root == null ) return true;
+        
+        if( !isValidBST(root.left) )
+            return false;
+        
+        if( root.val <= prev ){
+            return false;
+        }
+        prev = root.val;
+        
+        return isValidBST(root.right);
+        
     }
 }
